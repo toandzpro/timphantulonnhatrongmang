dbbe90f005bc3aeb6f7ae17857722cc620c193df
toandzpro <kimthoai15620@gmail.com>
2024-05-24 06:14:43 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+public class Baitap19
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        int[,] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+        int max = matrix[0, 0];
+        int row = 0, col = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] > max)
+                {
+                    max = matrix[i, j];
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        Console.WriteLine($"Giá trị lớn nhất trong ma trận là {max} tại tọa độ [{row}, {col}].");
+    }
+}
+
+
+
